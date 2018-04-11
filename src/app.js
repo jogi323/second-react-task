727f0b6e78a367e611a1cb47b88d0e5fdd642bce
Joginaidu Gopisetti <jgopisetti@INNOMINDSBANG.COM>
2018-04-11 10:38:10 +0530

    made setup

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 class App extends React.Component {
     render() { 
         return (  
-            <h1>Hellojhgkjh</h1>
+            <h1>Hello</h1>
         )
     }
 }
